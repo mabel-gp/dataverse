54238db177b19341572e7ddc7a404b311ed518fc
unknown <dorahuairesolis@gmail.com>
2024-08-02 19:40:15 -0500

    cambios para borrar

--- src/view.js
+++ src/view.js
@@ -1,6 +1,16 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+
+  const listaDeFlores = document.createElement("ul");
+
+  for(let i=0; i<data.length;i++) {
+    const flor = document.createElement("li");
+    flor.innerHTML = data[i];
+    listaDeFlores.appendChild(flor);
+  }
+    
+
+  console.log (listaDeFlores);
 };
 
