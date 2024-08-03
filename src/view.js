c6bf7bf4735a51636a91df8c3a2d6e199dcdb24e
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-08-02 19:22:17 -0500

    Agregar visualizacion de datos. En proceso

--- src/view.js
+++ src/view.js
@@ -1,6 +1,23 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  const listaDeFlores = document.createElement("ul");
+  for(let i=0; i<data.length;i++) {
+    const flor = document.createElement("li");
+    flor.innerHTML = data[i].name;
+    listaDeFlores.appendChild(flor);
+  }
+
+  const contenedorFlores = document.querySelector("#root");
+  contenedorFlores.appendChild(listaDeFlores);
+  // console.log (listaDeFlores);
+  return listaDeFlores;
 };
+// const contenedorFlores = document.querySelector("#root");
+// contenedorFlores.appendChild(listaDeFlores);
+// const lista = renderItems(data);
+// contenedorFlores.innerHTML;   //(listaDeFlores);
+// contenedorFlores.querySelector("#contenedor-flores").innerHTML;
+// contenedorFlores.innerHTML(listaDeFlores);
+
 
