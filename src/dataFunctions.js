0f326a79e053a6863afbce6ae436ea7a605b44ed
unknown <dorahuairesolis@gmail.com>
2024-08-08 23:13:32 -0500

    Resumir dataFunction

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,32 +1,11 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
 export const filterDataHabitat = (data, filterBy, value) => {
-  
-  const filtradasPorHabitat = data.filter((florHabitat) => {
-    if(value === "Acuática"){
-      return florHabitat.facts[filterBy] === "Acuática";
-    }else if(value === "Terrestre"){
-      return florHabitat.facts[filterBy] === "Terrestre";
-    }else if(value === "Epífita"){
-      return florHabitat.facts[filterBy] === "Epífita";
-    }else{
-      return 'No hay datos para mostrar';
-    }
-  });
-  return(filtradasPorHabitat);
+  return data.filter((filtradasPorHabitat) => filtradasPorHabitat.facts[filterBy] === value)
 };
 
 export const filterDataTamaño = (data, filterBy, value) => {
-  const filtradasPorTamaño = data.filter((florTamaño) => {
-    if(value === "Pequeña"){
-      return florTamaño.facts[filterBy] === "Pequeña";
-    }else if(value === "Mediana"){
-      return florTamaño.facts[filterBy] === "Mediana";
-    }else if(value === "Grande"){
-      return florTamaño.facts[filterBy] === "Grande";
-    }else{
-      return 'No hay datos para mostrar';
-    }
-  });
-  return(filtradasPorTamaño);
+  return data.filter((filtradasPorTamaño) => filtradasPorTamaño.facts[filterBy] === value)  
+  
 };
+
