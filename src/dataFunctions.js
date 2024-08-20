b86a758c3239608bfec504b0de6df840746d098a
unknown <dorahuairesolis@gmail.com>
2024-08-20 17:32:18 -0500

    cambiar funciones de filtro

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,15 +1,7 @@
 
 export const filterDataHabitat = (data, filterBy, value) => {
   const filtradasPorHabitat = data.filter((florHabitat) => {
-    if(value === "Acuática"){
-      return florHabitat.facts[filterBy] === "Acuática";
-    }else if(value === "Terrestre"){
-      return florHabitat.facts[filterBy] === "Terrestre";
-    }else if(value === "Epífita"){
-      return florHabitat.facts[filterBy] === "Epífita";
-    }else{
-      return 'No hay datos para mostrar';
-    }
+    return florHabitat.facts[filterBy] === value;
   });
   return filtradasPorHabitat;
 };
@@ -17,15 +9,7 @@ export const filterDataHabitat = (data, filterBy, value) => {
 
 export const filterDataTamaño = (data, filterBy, value) => {
   const filtradasPorTamaño = data.filter((florTamaño) => {
-    if(value === "Pequeña"){
-      return florTamaño.facts[filterBy] === "Pequeña";
-    }else if(value === "Mediana"){
-      return florTamaño.facts[filterBy] === "Mediana";
-    }else if(value === "Grande"){
-      return florTamaño.facts[filterBy] === "Grande";
-    }else{
-      return 'No hay datos para mostrar';
-    }
+    return florTamaño.facts[filterBy] === value;
   });
   return filtradasPorTamaño;
 };
